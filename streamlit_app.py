6c27dd9404db89e4db74cbeaf20d4da8127f3cf9
arvisptka <91487270+arvisptka@users.noreply.github.com>
2024-10-04 11:15:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title('🏨 Hotel Machine Learning 🏨')
 
 st.info('This is fisrt project machine learning with prediction')
+
+# load dataset
+df = pd.read_csv("https://raw.githubusercontent.com/arvisptka/Hotel_cancellationPrediction/refs/heads/main/Hotel%20Analysis%20Cancellation/datahotel_ec%2B.csv")
+df
