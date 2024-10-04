e02d2f351bbe968f9687e15c0c76bee566782bb0
arvisptka <91487270+arvisptka@users.noreply.github.com>
2024-10-04 17:24:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ with st.expander('Input Feature'):
 # Encode
 encode = ['Grouping_country', 'reservation_status']
 df_hotels = pd.get_dummies(input_cshotel, prefix=encode) 
-df_hotels[:]
+df_hotels[:1]
